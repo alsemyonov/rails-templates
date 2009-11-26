7f44236bdeb9c0b6e03bb5e8312782813624c649
Alexander Semyonov <rotuka@rotuka.com>
2009-11-26 16:25:07 +0300

    some changes to generic recipe

--- generic.rb
+++ generic.rb
@@ -1,19 +1,23 @@
-run "echo TODO > README"
+run 'echo TODO > README'
 
+# Configuring git
 file '.gitignore', <<-FILE
+*.log
 *.sqlite3
+*.sw[opn]
 .DS_store
+coverage
 doc/api
 doc/app
 doc/plugins
-log/*.log
 public/cache/**/*
+public/uploads
 tmp/**/*
+tmp/test
 vendor/gems
 FILE
 
-run 'touch tmp/.gitignore log/.gitignore vendor/.gitignore'
-run "cp config/database.yml config/example_database.yml"
+run 'touch {tmp,log,vendor}/.gitignore tmp/{cache,pids,sessions,sockets}/.gitignore'
 run 'rm public/index.html'
 
 git :init
@@ -21,58 +25,42 @@ git :add => '.'
 git :commit => '-am "Generic rails application"'
 
 # Adding some basic functionality
-gem 'haml'
+gem 'formtastic'
+gem 'inherited_resources'
 gem 'searchlogic'
-gem 'chriseppstein-compass', :lib => 'compass', :source => 'http://gems.github.com/'
-gem 'josevalim-inherited_resources', :lib => 'inherited_resources', :source => 'http://gems.github.com/'
-gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com/'
-gem 'sugar', :source => 'http://gemcutter.org/'
-gem 'espresso', :source => 'http://gemcutter.org/'
-gem 'yaroslav-russian', :lib => 'russian', :source => 'http://gems.github.com/'
-
+gem 'will_paginate'
+gem 'russian'
 git :add => '.'
-git :commit => '-am "WillPaginate and Searchlogic support built in"'
-
-# Adding HAML and SASS
-file 'vendor/plugins/haml/init.rb', <<-FILE
-begin
-  require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
-rescue LoadError
-  require 'haml' # From gem
-end
-
-# Load Haml and Sass
-Haml.init_rails(binding)
-FILE
+git :commit => '-am "WillPaginate, Searchlogic, InheritedResources and Russian support built in"'
 
 # Adding Compass
-run 'compass --rails -f blueprint . --sass-dir=app/stylesheets --css-dir=public/stylesheets/compiled'
-
+gem 'compass'
+gem 'haml'
+run 'compass --rails . --sass-dir=app/stylesheets --css-dir=public/stylesheets'
 file 'app/views/layouts/application.html.haml', <<-FILE
 !!! HTML5
 %html#nojs{html_attrs('ru-RU')}
   %head
     %meta(http-equiv="Content-type" content="text/html; charset=UTF-8")
-    %title= [page_title, t('application.title')].compact * '. '
+    %title= [page_title, t('application.meta.title')].compact * '. '
   %body
     %header.b_header
-      %h1.b_logo= t('application.logo')
+      %h1.b_logo= t('application.meta.title')
     %nav.b_nav
-      %ul= main_navigation
-    %content.b_content
+      %ul
+        %li= link_to('Home', '/')
+    %section.b_content
       = yield
     %footer.b_footer
-      %p.b_copyrights= t('application.copyrights')
+      %p.b_copyrights= t('application.meta.copyrights')
 FILE
 git :add => '.'
 git :commit => '-am "HAML, SASS and Blueprint through Compass"'
 
-plugin 'annotate_models', :git => 'git://github.com/rotuka/annotate_models.git'
-plugin 'irs_process_scripts', :git => 'git://github.com/rails/irs_process_scripts.git'
 run 'capify .'
 file 'config/deploy.rb', %q{
 set :application, 'application_name_here'
-set :user, 'desmix'
+set :user, 'rotuka'
 set :domain, 'eu107.activeby.net'
 
 set :scm, :git
@@ -104,11 +92,9 @@ RewriteRule ^(.*)$ dispatch.fcgi [QSA,L]
 
 ErrorDocument 500 "<h2>Application error</h2>Rails application failed to start properly"
 FILE
+plugin 'irs_process_scripts', :git => 'git://github.com/rails/irs_process_scripts.git'
 
 git :add => '.'
 git :commit => '-am "FastCGI Dispatchers and Apache Magic"'
 
-load_template "http://github.com/rotuka/rails-templates/raw/master/authlogic.rb" if yes?("Add AuthLogic authentication?")
-
-# Installing gems
-rake 'gems:install', :sudo => true
+load_template "http://github.com/rotuka/rails-templates/raw/master/authlogic.rb" if yes?("Add AuthLogic authentication? (y/n)")
